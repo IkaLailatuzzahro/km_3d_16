eb3f1dd3c573f4019702d1ca7288b73c36a3e63b
IkaLailatuzzahro <ikalailatuzzahrojoe@gmail.com>
2021-03-13 21:19:04 +0700

    modul 2

---
+++ src/pertemuan 2/Button UI/Assets/MenuActions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour
+{
+    public void MENU_ACTION_GotoPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+   // void Update()
+    //{
+        
+    //}
+}
